a62a5ea08040682caada8933166adf24abb4666b
erick <erick@erick-lap.(none)>
2013-05-05 20:50:25 -0500

    generator

--- lib/generators/media_include/install_generator.rb
+++ lib/generators/media_include/install_generator.rb
@@ -2,6 +2,7 @@ require 'rails/generators'
 module MediaInclude
   module Generators
     class InstallGenerator < Rails::Generators::Base
+    
       desc "Crea tabla images"
   
       # Commandline options can be defined here using Thor-like options:
@@ -10,14 +11,14 @@ module MediaInclude
       # I can later access that option using:
       # options[:my_opt]
   
-  
+      ##Inicializa el generador para ser usado en la app
       def self.source_root
-        puts "Hola a todos:)"
-        #@source_root ||= File.join(File.dirname(__FILE__), 'templates')
+        @source_root ||= File.join(File.dirname(__FILE__), 'templates')
       end
+      ## Your code goes here ;)  
+        puts "Hola a todos2 :)"
   
       # Generator Code. Remember this is just suped-up Thor so methods are executed in order
-  
   
     end
   end
--- media_include.gemspec
+++ media_include.gemspec
@@ -1,8 +1,6 @@
 # -*- encoding: utf-8 -*-
-##
-#lib = File.expand_path('../lib', __FILE__)
-#$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-$:.push File.expand_path("../lib", __FILE__)
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'media_include/version'
 
 Gem::Specification.new do |gem|
